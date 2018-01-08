6bacd12506c1e30df766f3b3ab74207cdcfc1ae7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-01-08 08:28:31 +0100

    Added model for redirect display

---
+++ Source/Our.Umbraco.RedirectsViewer/Models/RedirectDisplay.cs
@@ -0,0 +1,29 @@
+﻿namespace Our.Umbraco.RedirectsViewer.Models
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The redirect display model
+    /// </summary>
+    [DataContract(Name = "redirect", Namespace = "")]
+    public class RedirectDisplay
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the old url.
+        /// </summary>
+        [DataMember(Name = "url")]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the create date.
+        /// </summary>
+        [DataMember(Name = "createDate")]
+        public string CreateDate { get; set; }
+    }
+}
