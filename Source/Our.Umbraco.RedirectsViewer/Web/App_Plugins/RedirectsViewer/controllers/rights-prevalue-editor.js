66c48c4a3b197bc20c6c928d57f8928aa828a7fd
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-01 16:30:49 +0100

    Added controller for rights prevalue editor

---
+++ Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/controllers/rights-prevalue-editor.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    "use strict";
+
+    function RightsPrevalueEditorController($scope) {
+        var vm = this;
+
+        vm.allowed = false;
+
+        if ($scope.model.value) {
+            console.log("we have a value");
+            vm.allowed = $scope.model.value.allowed;
+        }
+
+        //sync things up on save
+        $scope.$on("formSubmitting", function (ev, args) {
+            console.log(ev);
+            console.log(args);
+            $scope.model.value = {
+                allowed : vm.allowed
+            }
+        });
+
+        console.log($scope.model);
+    }
+
+    angular.module("umbraco").controller("Our.Umbraco.RedirectsViewer.RightsPrevalueEditorController", RightsPrevalueEditorController);
+
+})();
