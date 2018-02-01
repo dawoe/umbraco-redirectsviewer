29a867656f0bc2b029bc38b2cf663e2e91a80dd8
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-01 19:09:37 +0100

    Added user group mapping

---
+++ Source/Our.Umbraco.RedirectsViewer/Mapping/UsergroupMapping.cs
@@ -0,0 +1,30 @@
+﻿namespace Our.Umbraco.RedirectsViewer.Mapping
+{
+    using AutoMapper;
+
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Models.Mapping;
+    using global::Umbraco.Core.Models.Membership;
+
+    using Our.Umbraco.RedirectsViewer.Models;
+
+    /// <summary>
+    /// The usergroup mapping.
+    /// </summary>
+    internal class UsergroupMapping : MapperConfiguration
+    {
+        /// <summary>
+        /// Configures the user group mappings
+        /// </summary>
+        /// <param name="config">
+        /// The config.
+        /// </param>
+        /// <param name="applicationContext">
+        /// The application context.
+        /// </param>
+        public override void ConfigureMappings(IConfiguration config, ApplicationContext applicationContext)
+        {
+            config.CreateMap<IUserType, UserGroupDisplay>();
+        }
+    }
+}
