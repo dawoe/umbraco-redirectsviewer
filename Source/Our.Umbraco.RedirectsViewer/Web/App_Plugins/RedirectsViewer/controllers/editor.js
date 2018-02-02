dfdd69d38596546d7bc8f5dfc52eef9d343bb358
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-02 12:09:03 +0100

    Created controller for the editor

---
+++ Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/controllers/editor.js
@@ -0,0 +1,15 @@
+﻿(function() {
+    "use strict";
+
+    function EditController($scope, $routeParams) {
+        var vm = this;
+
+        vm.isCreate = false;
+        vm.isLoading = true;
+
+
+    }
+
+    angular.module("umbraco").controller("Our.Umbraco.RedirectsViewer.EditController",
+        ['$scope', '$routeParams', EditController]);
+})();
