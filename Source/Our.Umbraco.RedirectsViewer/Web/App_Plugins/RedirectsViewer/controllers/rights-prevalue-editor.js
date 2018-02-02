d8b2bd77d39f461fe9501a05952e5b2cf1c5744a
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-02 09:41:05 +0100

    Load user groups in controller

--- Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/controllers/rights-prevalue-editor.js
+++ Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/controllers/rights-prevalue-editor.js
@@ -1,28 +1,45 @@
 ﻿(function () {
     "use strict";
 
-    function RightsPrevalueEditorController($scope) {
+    function RightsPrevalueEditorController($scope, userGroupResource) {
         var vm = this;
 
+        vm.loading = true;
         vm.allowed = false;
+        vm.groups = [];
+        vm.selectedGroups = [];
 
-        if ($scope.model.value) {
-            console.log("we have a value");
+
+        if ($scope.model.value) {          
             vm.allowed = $scope.model.value.allowed;
         }
 
         //sync things up on save
-        $scope.$on("formSubmitting", function (ev, args) {
-            console.log(ev);
-            console.log(args);
+        $scope.$on("formSubmitting", function (ev, args) {            
             $scope.model.value = {
-                allowed : vm.allowed
+                allowed: vm.allowed,
+                selectedRights : vm.selectedRights
             }
         });
 
-        console.log($scope.model);
+       
+        function init() {
+            if ($scope.model.value) {
+                vm.allowed = $scope.model.value.allowed;
+                vm.selectedRights = $scope.model.value.usergroups;
+            }
+
+            userGroupResource.getAll().then(
+                function(data) {                   
+                    vm.groups = data;
+                    vm.loading = false;
+                }
+            );
+        }
+
+        init();
     }
 
-    angular.module("umbraco").controller("Our.Umbraco.RedirectsViewer.RightsPrevalueEditorController", RightsPrevalueEditorController);
+    angular.module("umbraco").controller("Our.Umbraco.RedirectsViewer.RightsPrevalueEditorController", ['$scope', 'Our.Umbraco.RedirectsViewer.UserGroupResource', RightsPrevalueEditorController]);
 
 })();
