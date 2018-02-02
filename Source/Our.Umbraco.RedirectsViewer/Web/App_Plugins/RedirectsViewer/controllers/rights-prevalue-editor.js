c8cda0b6ce99d75a454bee6162b42c34f92f5bb9
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-02 10:35:36 +0100

    Save user group selection in editor

--- Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/controllers/rights-prevalue-editor.js
+++ Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/controllers/rights-prevalue-editor.js
@@ -15,10 +15,17 @@
         }
 
         //sync things up on save
-        $scope.$on("formSubmitting", function (ev, args) {            
+        $scope.$on("formSubmitting", function (ev, args) {
+
+            var selectedGroups = _.filter(vm.groups, function (x) { return x.checked === true });
+           
+            var selectedAliases = _.map(selectedGroups, function (x) { return x.alias });
+
+            vm.selectedGroups = selectedAliases;
+
             $scope.model.value = {
                 allowed: vm.allowed,
-                selectedRights : vm.selectedRights
+                usergroups: vm.selectedGroups
             }
         });
 
@@ -26,16 +33,24 @@
         function init() {
             if ($scope.model.value) {
                 vm.allowed = $scope.model.value.allowed;
-                vm.selectedRights = $scope.model.value.usergroups;
+                vm.selectedGroups = $scope.model.value.usergroups;
             }
 
             userGroupResource.getAll().then(
                 function(data) {                   
                     vm.groups = data;
+                    applySelection();
                     vm.loading = false;
                 }
             );
         }
+
+        function applySelection() {
+            for (var i = 0; i < vm.groups.length; i++) {
+                var isChecked = _.contains(vm.selectedGroups, vm.groups[i].alias);
+                vm.groups[i].checked = isChecked;
+            }
+        };       
 
         init();
     }
