1ceb5f3a695b65995ed62f94638145949c71df58
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-02 12:45:37 +0100

    Show message when in create mode or when redirects are disabled

--- Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/controllers/editor.js
+++ Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/controllers/editor.js
@@ -1,15 +1,38 @@
 ﻿(function() {
     "use strict";
 
-    function EditController($scope, $routeParams) {
+    function EditController($scope, $routeParams, redirectUrlsResource) {
         var vm = this;
 
-        vm.isCreate = false;
+        vm.isCreate = $routeParams.create;
         vm.isLoading = true;
+        vm.isEnabled = false;
+        vm.isAdmin = false;
 
+        function checkEnabled() {
+            return redirectUrlsResource.getEnableState().then(function(data) {
+                vm.isEnabled = data.enabled === true;
+                vm.isAdmin = data.userIsAdmin;
 
+                if (vm.isEnabled === false) {
+                    vm.isLoading = false;
+                }
+
+                console.log(vm.isAdmin);
+            });
+        };
+
+        vm.checkEnabled = checkEnabled;
+
+        function init() {
+            vm.checkEnabled().then(function() {
+                console.log("Enabled check completed");
+            });
+        };
+
+        init();
     }
 
     angular.module("umbraco").controller("Our.Umbraco.RedirectsViewer.EditController",
-        ['$scope', '$routeParams', EditController]);
+        ['$scope', '$routeParams', 'redirectUrlsResource', EditController]);
 })();
