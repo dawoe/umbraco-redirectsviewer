2e03afcbb19c31771488136087fba7b2cd866591
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-02 09:11:48 +0100

    Added user groups angular resource

---
+++ Source/Our.Umbraco.RedirectsViewer/Web/App_Plugins/RedirectsViewer/resources/usergroup-resource.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    "use strict";
+
+    function UserGroupResource($http, umbRequestHelper) {
+
+        var resource = {
+            getAll: getAllUserGroups
+        };
+
+        return resource
+
+        function getAllUserGroups() {
+            return umbRequestHelper.resourcePromise(
+                $http.get(umbRequestHelper.getApiUrl("Umbraco.Sys.ServerVariables.Our.Umbraco.RedirectsViewer", "GetUserGroups")), "Failed to get list");
+        }
+
+    }
+
+    angular.module("umbraco.resources").factory("Our.Umbraco.RedirectsViewer.UserGroupResource", UserGroupResource);
+
+})();
