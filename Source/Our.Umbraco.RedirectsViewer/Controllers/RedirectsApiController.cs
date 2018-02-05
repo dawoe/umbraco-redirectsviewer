e4ab3de2566438b6580835fef70d3afc4b55c7ae
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-05 12:00:54 +0100

    Refactored api controller so it becomes testable

--- Source/Our.Umbraco.RedirectsViewer/Controllers/RedirectsApiController.cs
+++ Source/Our.Umbraco.RedirectsViewer/Controllers/RedirectsApiController.cs
@@ -9,6 +9,8 @@
 
     using AutoMapper;
 
+    using global::Umbraco.Core.Services;
+    using global::Umbraco.Web;
     using global::Umbraco.Web.Editors;
     using global::Umbraco.Web.Models.ContentEditing;
 
@@ -17,6 +19,46 @@
     /// </summary>
     public class RedirectsApiController : BackOfficeNotificationsController
     {
+        /// <summary>
+        /// The redirect url service.
+        /// </summary>
+        private readonly IRedirectUrlService redirectUrlService;
+
+        /// <summary>
+        /// The mapper.
+        /// </summary>
+        private readonly IMappingEngine mapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedirectsApiController"/> class.
+        /// </summary>
+        public RedirectsApiController()
+        {
+            this.redirectUrlService = this.Services.RedirectUrlService;
+            this.mapper = Mapper.Engine;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RedirectsApiController"/> class.
+        /// </summary>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        /// <param name="redirectUrlService">
+        /// The redirect url service.
+        /// </param>
+        /// <param name="mapper">
+        /// The mapper.
+        /// </param>
+        public RedirectsApiController(
+            UmbracoContext context,
+            IRedirectUrlService redirectUrlService,
+            IMappingEngine mapper)
+        {
+            this.redirectUrlService = redirectUrlService;
+            this.mapper = mapper;
+        }
+
         /// <summary>
         /// Gets the redirects for the content
         /// </summary>
@@ -29,9 +71,7 @@
         [HttpGet]
         public HttpResponseMessage GetRedirectsForContent(Guid contentKey)
         {
-            var redirectService = this.Services.RedirectUrlService;
-
-            var redirects = Mapper.Map<IEnumerable<ContentRedirectUrl>>(redirectService.GetContentRedirectUrls(contentKey)).ToArray();
+            var redirects = this.mapper.Map<IEnumerable<ContentRedirectUrl>>(this.redirectUrlService.GetContentRedirectUrls(contentKey)).ToArray();
 
             return this.Request.CreateResponse(HttpStatusCode.OK, redirects);
         }
