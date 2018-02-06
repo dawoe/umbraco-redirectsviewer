f38524863b0df2a6145cdda2403997fde2983f81
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-02-06 13:30:57 +0100

    Added model for saving redirects

---
+++ Source/Our.Umbraco.RedirectsViewer/Models/RedirectSave.cs
@@ -0,0 +1,24 @@
+﻿namespace Our.Umbraco.RedirectsViewer.Models
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The redirect save.
+    /// </summary>
+    [DataContract(Name = "redirect")]
+    public class RedirectSave
+    {
+        /// <summary>
+        /// Gets or sets the url.
+        /// </summary>
+        [DataMember(Name = "url")]
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content key.
+        /// </summary>
+        [DataMember(Name = "contentKey")]
+        public Guid ContentKey { get; set; }
+    }
+}
