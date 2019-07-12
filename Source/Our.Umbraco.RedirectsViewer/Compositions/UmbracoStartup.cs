0fcc071f547765a6cb5b43ecf7fe0cb1230cf342
Ismail Mayat <ismailm123@gmail.com>
2019-07-12 15:46:59 +0100

    adding automapper mapping using profile

---
+++ Source/Our.Umbraco.RedirectsViewer/Compositions/UmbracoStartup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using Our.Umbraco.RedirectsViewer.Mapping;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Our.Umbraco.RedirectsViewer.Compositions
+{
+    public class UmbracoStartup:IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Register<Profile, UserGroupProfile>();
+        }
+    }
+}
---
+++ Source/Our.Umbraco.RedirectsViewer/Mapping/UserGroupProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Our.Umbraco.RedirectsViewer.Models;
+using Umbraco.Core.Models.Membership;
+
+namespace Our.Umbraco.RedirectsViewer.Mapping
+{
+    internal class UserGroupProfile:Profile
+    {
+        public UserGroupProfile()
+        {
+            CreateMap<IUserGroup, UserGroupDisplay>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Alias, opt => opt.MapFrom(src => src.Alias));
+        }
+    }
+}
