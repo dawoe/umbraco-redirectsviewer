b3cde36d4515409b18171a33a6e02a08f8a5c761
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-07-27 14:28:35 +0200

    Updated build process

--- Packaging/build-appveyor.bat
+++ Packaging/build-appveyor.bat
@@ -3,5 +3,4 @@ ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
 ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 
-Call Tools\Nuget.exe restore ..\Source\Our.Umbraco.RedirectsViewer\Our.Umbraco.RedirectsViewer.sln
-CALL "%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MsBuild.exe" build.xml
+CALL "%programfiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" build.xml
