b6b2cb294442e74d1efe6bba18953d3c64a94800
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-07-27 13:37:34 +0200

    Removed the test project...they are not run anymore

--- Source/Our.Umbraco.RedirectsViewer/Properties/AssemblyInfo.cs
+++ Source/Our.Umbraco.RedirectsViewer/Properties/AssemblyInfo.cs
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
-
-[assembly: InternalsVisibleTo("Our.Umbraco.RedirectsViewer.Tests")]
