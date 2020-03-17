62a2e2320c6e6ceacf274737cc99df70e773d570
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2020-03-17 13:11:44 +0100

    Moved server variable parsing to a component

---
+++ Source/Our.Umbraco.RedirectsViewer/Compositions/RedirectsViewerComposer.cs
@@ -0,0 +1,15 @@
+﻿using Umbraco.Core.Composing;
+using Umbraco.Core;
+using Our.Umbraco.RedirectsViewer.Components;
+
+namespace Our.Umbraco.RedirectsViewer.Compositions
+{
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    internal class RedirectsViewerComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<ServerVariableRegistrationComponent>();
+        }
+    }
+}
